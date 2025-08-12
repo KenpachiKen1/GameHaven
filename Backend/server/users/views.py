a41d75ed1fce5d342ab73db8ec58003cbd0abeca
Kenneth Fadojutimi <13kken@mac.mynetworksettings.com>
2025-08-12 17:27:25 -0400

    Started CRUD Development.

--- Backend/server/users/views.py
+++ Backend/server/users/views.py
@@ -11,14 +11,19 @@ from django.utils import timezone
 from django.views.decorators.csrf import csrf_exempt
 load_dotenv()
 import datetime
-#Will add specific
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+
+# Django imports
+
 
 def index(request):
     return HttpResponse("This is the start of something great!")
 
+#Going to be making CRUD related functions, already finished the create function
 
+
+# Create a user profile
 @csrf_exempt
 def create_user(request):
     try:
@@ -49,4 +54,47 @@ def create_user(request):
         }, status=201)
      # Would return a profile instance and I'd have access to profile.username etc.
 
-#typically, at least for this project, helper functions shouldn't return any sort of http or json response!
+#delete user function
+def delete_user(request):
+    try:
+        if request.method != 'DELETE':
+         return HttpResponse("This function is only for DELETE methods", status = 400)
+        
+        data = json.loads(request.body) #loading in the request information which is the account that is meant to be deleted.
+
+        if(User.objects.filter(username=data.get("userName"))).exists():
+            User.objects.filter(username=data.get("userName")).delete()
+            return HttpResponse("Deleted user successfully", status=200)
+        
+        return JsonResponse("No user was found", status=404)
+    except Exception as e:
+        raise HttpResponse(str(e), status=400)
+    
+
+#Update User Function
+
+def update_user(request):
+    try:
+        if request.method != 'PATCH':
+            return HttpResponse("This function is only for PATCH methods", status = 400)
+        data = json.loads(request.body)
+        user = get_object_or_404(User, id=data.get("id")) #checks to see if the user even exists, if not then throws a 404 error
+    #updating each necessary field.
+        if 'userName' in data:
+            user.username = data.get("userName") 
+        if 'firstName' in data:
+            user.first_name = data.get("firstName")
+        if 'lastName' in data:
+            user.last_name = data.get("lastName")
+        if 'email' in data:
+         user.email = data.get("email")
+
+        user.save() #saving the model. 
+        return HttpResponse("Updated successfully", status=200)
+    except Exception as e:
+       return HttpResponse(str(e), status=400)
+    #Once I develop the frontend a bit more, will add more fields like favorite game, profile photo etc.
+    #Will write a password validation check function later, this function will require the user to enter their password, they must correctly enter it in order to change their current password.
+
+    
+
